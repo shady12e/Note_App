c7bf6f0f2eab1b731cefd0766a397b26102d3f1f
hp <shadye400@gmail.com>
2024-12-07 00:24:21 +0200

    create note adapter

--- lib/model/note_item_model.dart
+++ lib/model/note_item_model.dart
@@ -1,7 +1,17 @@
-class NoteModel {
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'note_item_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+
+  @HiveField(0)
   final String title;
+   @HiveField(1)
   final String subtitle;
+   @HiveField(2)
   final String date;
+   @HiveField(3)
   final int color;
 
   NoteModel(
