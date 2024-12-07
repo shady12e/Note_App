d41f97ac987a9724d786a412a43c2030979fe386
hp <shadye400@gmail.com>
2024-12-07 17:18:40 +0200

    add fuction to add note model

--- lib/cubits/add_new_cubit/add_new_cubit_cubit.dart
+++ lib/cubits/add_new_cubit/add_new_cubit_cubit.dart
@@ -1,7 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/constes.dart';
+import 'package:note_app/model/note_item_model.dart';
 
 import 'add_new_cubit_state.dart';
 
 class AddNewCubitCubit extends Cubit<AddNewCubitState> {
   AddNewCubitCubit() : super(AddNewCubitInitial());
+
+  addNewNote(NoteModel note) async {
+    emit(AddNewCubitLouding());
+    try {
+      var notesBox = Hive.box<NoteModel>(knotesBox);
+      emit(AddNewCubitSucceful());
+      await notesBox.add(note);
+    } catch (e) {
+      emit(
+        AddNewCubitFuiler(e.toString()),
+      );
+    }
+  }
 }
